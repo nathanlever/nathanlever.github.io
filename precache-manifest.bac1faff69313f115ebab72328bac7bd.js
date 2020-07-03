0644b0183062cf4381c5ff186839c4737def828c
Nathan <lever.nathan@gmail.com>
2020-07-03 15:19:21 +0300

    Initial commit

---
+++ precache-manifest.bac1faff69313f115ebab72328bac7bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "400187841a013a799794ca23f9cb4e23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a92885e8c2d4ede8706",
+    "url": "/static/css/main.3788ac75.chunk.css"
+  },
+  {
+    "revision": "d14a225e3d9e085b1950",
+    "url": "/static/js/2.26234256.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.26234256.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a92885e8c2d4ede8706",
+    "url": "/static/js/main.591db4d7.chunk.js"
+  },
+  {
+    "revision": "bea266e306e08d97e8c2",
+    "url": "/static/js/runtime-main.9d7b5c1d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
